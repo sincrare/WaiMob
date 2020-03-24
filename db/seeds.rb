a5fcf35a1845973348d26beed46de630690b76f1
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-24 18:50:00 +0900

    seed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if User.count == 0 
+  User.create(email: "waimob@test.com", password: "aaaaaa", password_confirmation: "aaaaaa")
+end
+
+if Document.count == 0
+  Document.create(title: "first document")
+end
