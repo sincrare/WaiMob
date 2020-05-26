0f9f02ef0907f7bb61be44f2609b783445254414
ksdueki <ueki@aok-net.com>
2020-05-26 22:12:23 +0900

    bugfix 改行の同期

--- app/javascript/channels/document_channel.js
+++ app/javascript/channels/document_channel.js
@@ -16,7 +16,15 @@ $(function() {
     received: function (data) {
       const content = data['content'];
       const row_id = data['id'];
-      $(`#${row_id}`).text(content);
+      const position = data['position'];
+
+      if (position){
+        const previous_row = $(document.querySelectorAll('.js-document__row')[parseInt(position)-2])
+        const new_el = $(`<p id="${row_id}" class="js-document__row">${content}</p>`) 
+        previous_row.after(new_el)
+      }else{
+        $(`#${row_id}`).text(content);
+      }
     },
 
     append: function (chunk) {
