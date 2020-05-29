e709bdf984578071c4c3c6f3114e16f85709aec8
pi-chan <hiromasa.ohno.1007@gmail.com>
2020-05-29 11:21:01 +0900

    configure vue

---
+++ app/javascript/vue/init.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import VueDisableInterpolation from 'vue-disable-interpolation';
+import registerCustomElement from '@sonicgarden/vue-custom-element-rails';
+
+Vue.use(VueDisableInterpolation);
+
+// registerCustomElement('hoge-hoge', HogeHoge);
